110bc1a9959748e7816af0708b5e5251f0ca864c
jeongmin <jeongmin@jeongminui-iMac.local>
2020-09-21 18:42:34 +0900

    Add LayoutPratice

---
+++ frontend-study/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LayoutTest from "./components/LayoutTest";
+function App() {
+  return (
+    <div>
+      <LayoutTest>
+      </LayoutTest>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend-study/src/components/LayoutTest.js
@@ -0,0 +1,15 @@
+import React from "react";
+import LayoutTet from './LayoutTest.css';
+
+const LayoutTest = () => {
+    return (
+        <div className="TestTest">
+            <header>
+
+            </header>
+        </div>
+    )
+}
+
+
+export default LayoutTest;
